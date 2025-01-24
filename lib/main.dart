1ee6f4fe9fe5cd5213a2643789f588b833a7f032
ASD-9 <ryan.chavatte@gmail.com>
2025-01-24 17:19:30 +0100

    create base theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mobile_app/routes/app_routes.dart';
+import 'package:mobile_app/themes/app_theme.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: darkTheme,
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.initialRoute,
       routes: AppRoutes.routes,
---
+++ lib/themes/app_color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primaryColor = Color(0xFF121212);
+  static const Color secondaryColor = Color(0xFF1E1E1E);
+  static const Color primaryAccentColor = Color(0xFFBB86FC);
+  static const Color secondaryAccentColor = Color(0xFF03DAC6);
+  static const Color errorColor = Color(0xFFCF6679);
+  static const Color primaryTextColor = Color(0xFFFFFFFF);
+  static const Color secondaryTextColor = Color(0xFFB0B0B0);
+  static const Color disabledColor = Color(0xFF666666);
+  static const Color dividerColor = Color(0xFF2E2E2E);
+  static const Color borderColor = Color(0xFF444444);
+}
