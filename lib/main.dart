c918e3eac429192d0a972cb623714757752dba60
ASD-9 <ryan.chavatte@gmail.com>
2025-01-24 15:54:24 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:mobile_app/routes/app_routes.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.initialRoute,
+      routes: AppRoutes.routes,
+    );
+  }
+}
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  static const initialRoute = "";
+
+  static Map<String, WidgetBuilder> routes = {};
+}
